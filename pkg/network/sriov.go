b7699053d198a5063837e827674d7c55326ccad9
Ihar Hrachyshka <ihar@redhat.com>
2019-03-22 15:28:07 -0700

    Deploy sriov components
    Fixes #11
    
    This change does *not* create a network for SR-IOV (left for later
    patches).
    
    This change also doesn't address heterogeneous deployments where root
    devices of SR-IOV network devices are different. (Device plugin requires
    that admin explicitly lists each root device in a configuration file
    local to each node.)

---
+++ pkg/network/sriov.go
@@ -0,0 +1,42 @@
+package network
+
+import (
+	"os"
+	"path/filepath"
+	"reflect"
+
+	"github.com/kubevirt/cluster-network-addons-operator/pkg/render"
+	"github.com/pkg/errors"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+
+	opv1alpha1 "github.com/kubevirt/cluster-network-addons-operator/pkg/apis/networkaddonsoperator/v1alpha1"
+)
+
+func changeSafeSriov(prev, next *opv1alpha1.NetworkAddonsConfigSpec) []error {
+	if prev.Sriov != nil && !reflect.DeepEqual(prev.Sriov, next.Sriov) {
+		return []error{errors.Errorf("cannot modify Sriov configuration once it is deployed")}
+	}
+	return nil
+}
+
+// renderSriov generates the manifests of SR-IOV plugins
+func renderSriov(conf *opv1alpha1.NetworkAddonsConfigSpec, manifestDir string, enableSCC bool) ([]*unstructured.Unstructured, error) {
+	if conf.Sriov == nil {
+		return nil, nil
+	}
+
+	// render the manifests on disk
+	data := render.MakeRenderData()
+	data.Data["SriovRootDevices"] = os.Getenv("SRIOV_ROOT_DEVICES")
+	data.Data["SriovDpImage"] = os.Getenv("SRIOV_DP_IMAGE")
+	data.Data["SriovCniImage"] = os.Getenv("SRIOV_CNI_IMAGE")
+	data.Data["ImagePullPolicy"] = os.Getenv("IMAGE_PULL_POLICY")
+	data.Data["EnableSCC"] = enableSCC
+
+	objs, err := render.RenderDir(filepath.Join(manifestDir, "sriov"), &data)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to render sriov manifests")
+	}
+
+	return objs, nil
+}
