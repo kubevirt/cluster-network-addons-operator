bfd5ce197f6bea729591dad12f56d77711c33932
Petr Horacek <phoracek@redhat.com>
2019-04-22 13:51:28 +0200

    update manifests in README.md during release
    Signed-off-by: Petr Horacek <phoracek@redhat.com>

--- hack/release.sh
+++ hack/release.sh
@@ -2,10 +2,11 @@
 
 set -e
 
-released_version=$1
-future_version=$2
-origin_remote=$3
-fork_remote=$4
+previous_version=$1
+released_version=$2
+future_version=$3
+origin_remote=$4
+fork_remote=$5
 
 echo 'Fetching origin'
 git fetch ${origin_remote}
@@ -18,6 +19,13 @@ VERSION=${released_version} IMAGE_TAG=${released_version} make generate-manifest
 
 echo 'Add new manifests to the source tree'
 git add manifests/
+
+echo 'Upgrade README.md with the released manifests'
+sed -i "s/\(.*kubectl apply.*\)${previous_version}\(.*\)/\1${released_version}\2/g" README.md
+sed -i "s/\(.*startingCSV.*\)${previous_version}\(.*\)/\1${released_version}\2/g" README.md
+
+echo 'Add updated readme to the source tree'
+git add README.md
 
 echo 'Commit new manifests, this commit is to be tagged with the new release'
 git commit -s -m "release ${released_version} - update manifests"
