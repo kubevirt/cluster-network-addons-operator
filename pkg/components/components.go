90541ad9b9c754df6b576e3dac3f1a6a09428090
Petr Horacek <phoracek@redhat.com>
2019-05-16 13:02:35 +0200

    fix nmstate version to 0.1
    We need to use fixed images version, otherwise, a change in :latest
    component's image might be devastating in combination with manifests
    shipped by us.
    
    Signed-off-by: Petr Horacek <phoracek@redhat.com>

--- pkg/components/components.go
+++ pkg/components/components.go
@@ -20,7 +20,7 @@ const (
 	SriovDpImageDefault             = "quay.io/kubevirt/cluster-network-addon-sriov-device-plugin:v2.0.0-1.git9a20829"
 	SriovCniImageDefault            = "quay.io/kubevirt/cluster-network-addon-sriov-cni:v1.1.0-1.git9e4c973"
 	KubeMacPoolImageDefault         = "quay.io/kubevirt/kubemacpool:v0.3.0"
-	NMStateStateHandlerImageDefault = "quay.io/nmstate/kubernetes-nmstate-state-handler:latest"
+	NMStateStateHandlerImageDefault = "quay.io/nmstate/kubernetes-nmstate-state-handler:v0.1.0"
 )
 
 type AddonsImages struct {
