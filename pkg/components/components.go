dccc65b5907e4344d801065ca62c57986db6c9ef
Sebastian Sch <sebassch@gmail.com>
2019-05-30 11:15:11 +0300

    Bump linux bridge to v0.2.0
    This PR update the the linux_bridge image to use cni plugin version v0.8.0
    
    CNI plugin version v0.8.0 contains the vlan feature for linux bridge

--- pkg/components/components.go
+++ pkg/components/components.go
@@ -15,7 +15,7 @@ const Name = "cluster-network-addons-operator"
 
 const (
 	MultusImageDefault              = "quay.io/kubevirt/cluster-network-addon-multus:v3.2.0-1.gitbf61002"
-	LinuxBridgeCniImageDefault      = "quay.io/kubevirt/cni-default-plugins:v0.1.0"
+	LinuxBridgeCniImageDefault      = "quay.io/kubevirt/cni-default-plugins:v0.2.0"
 	LinuxBridgeMarkerImageDefault   = "quay.io/kubevirt/bridge-marker:0.1.0"
 	SriovDpImageDefault             = "quay.io/kubevirt/cluster-network-addon-sriov-device-plugin:v2.0.0-1.git9a20829"
 	SriovCniImageDefault            = "quay.io/kubevirt/cluster-network-addon-sriov-cni:v1.1.0-1.git9e4c973"
