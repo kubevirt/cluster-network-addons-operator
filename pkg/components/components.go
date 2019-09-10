c4ff24b8c9d38cd23c5030eb9937bbb07dbebc0b
Quique Llorente <ellorent@redhat.com>
2019-09-10 14:28:34 +0200

    Upgrade kubernetes-nmstate-handler container to 0.8.0
    - Support untagged vlan filtering linux bridge traffic
    - Increase nmstate set timeout to 60 seconds

--- pkg/components/components.go
+++ pkg/components/components.go
@@ -23,7 +23,7 @@ const (
 	LinuxBridgeCniImageDefault    = "quay.io/kubevirt/cni-default-plugins:v0.8.1"
 	LinuxBridgeMarkerImageDefault = "quay.io/kubevirt/bridge-marker:0.2.0"
 	KubeMacPoolImageDefault       = "quay.io/kubevirt/kubemacpool:v0.5.0"
-	NMStateHandlerImageDefault    = "quay.io/nmstate/kubernetes-nmstate-handler:v0.6.0"
+	NMStateHandlerImageDefault    = "quay.io/nmstate/kubernetes-nmstate-handler:v0.8.0"
 	OvsCniImageDefault            = "quay.io/kubevirt/ovs-cni-plugin:v0.7.0"
 	OvsMarkerImageDefault         = "quay.io/kubevirt/ovs-cni-marker:v0.7.0"
 )
