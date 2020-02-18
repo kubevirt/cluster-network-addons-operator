eaa7fb09fd07bd662b5542765b4329343afae4c3
Quique Llorente <ellorent@redhat.com>
2020-02-18 14:36:08 +0100

    Bump knmstate to v0.15.1
    Signed-off-by: Quique Llorente <ellorent@redhat.com>

--- test/releases/0.28.0.go
+++ test/releases/0.28.0.go
@@ -36,13 +36,13 @@ func init() {
 				ParentName: "nmstate-handler",
 				ParentKind: "DaemonSet",
 				Name:       "nmstate-handler",
-				Image:      "quay.io/nmstate/kubernetes-nmstate-handler:v0.14.0",
+				Image:      "quay.io/nmstate/kubernetes-nmstate-handler:v0.15.1",
 			},
 			opv1alpha1.Container{
 				ParentName: "nmstate-handler-worker",
 				ParentKind: "DaemonSet",
 				Name:       "nmstate-handler",
-				Image:      "quay.io/nmstate/kubernetes-nmstate-handler:v0.14.0",
+				Image:      "quay.io/nmstate/kubernetes-nmstate-handler:v0.15.1",
 			},
 			opv1alpha1.Container{
 				ParentName: "ovs-cni-amd64",
