4e60154acfcfb760f49de252769b18dd4a530df8
Or Shoval <oshoval@redhat.com>
2020-02-19 13:49:03 +0200

    Fix ocp make cluster-sync
    Signed-off-by: Or Shoval <oshoval@redhat.com>

--- cluster/up.sh
+++ cluster/up.sh
@@ -21,7 +21,7 @@ kubevirtci::install
 
 $(kubevirtci::path)/cluster-up/up.sh
 
-if [[ "$KUBEVIRT_PROVIDER" =~ okd- ]]; then
+if [[ "$KUBEVIRT_PROVIDER" =~ (ocp|okd)- ]]; then
     echo 'Remove components we do not need to save some resources'
     ./cluster/kubectl.sh delete ns openshift-monitoring --wait=false
     ./cluster/kubectl.sh delete ns openshift-marketplace --wait=false
