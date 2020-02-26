f49e511e7604a66ba04631f5f0945f1ac40788ae
Petr Horacek <phoracek@redhat.com>
2020-02-26 13:14:51 +0100

    release 0.27.2 - update makefile
    Signed-off-by: Petr Horacek <phoracek@redhat.com>

--- test/releases/0.27.2.go
+++ test/releases/0.27.2.go
@@ -6,7 +6,7 @@ import (
 
 func init() {
 	release := Release{
-		Version: "0.28.0",
+		Version: "0.27.2",
 		Containers: []opv1alpha1.Container{
 			opv1alpha1.Container{
 				ParentName: "multus",
