a2df64bfef4bf0da3751fdcfc970405e164fe742
Petr Horacek <phoracek@redhat.com>
2020-03-06 14:07:11 +0100

    automate GitHub releasing (#330)
    With this PR, GitHub release tagging can be automated (via Prow). This means
    we don't need to draft new releases after release PR gets merged manually.
    
    Signed-off-by: Petr Horacek <phoracek@redhat.com>

---
+++ hack/release.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -e
+
+git tag $TAG
+git push https://github.com/kubevirt/cluster-network-addons-operator $TAG
+
+$GITHUB_RELEASE release -u kubevirt -r cluster-network-addons-operator \
+    --tag $TAG \
+    --name $TAG \
+    --description "$(cat $DESCRIPTION)"
+
+for resource in "$@" ;do
+    $GITHUB_RELEASE upload -u kubevirt -r cluster-network-addons-operator \
+        --name $(basename $resource) \
+        --tag $TAG \
+        --file $resource
+done
