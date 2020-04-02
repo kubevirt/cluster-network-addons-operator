745dbf34effb8cdf124b4a8e5ecdcde865470b05
Petr Horacek <phoracek@redhat.com>
2020-04-02 16:03:50 +0200

    don't remove CRDs when their comonents are gone (#357)
    * don't apply owner reference on CRDs
    
    We want to prevent deletion of CRs created by the user, even during removal of
    the operator. Therefore, we don't set ownership reference on CRDs.
    
    Signed-off-by: Petr Horacek <phoracek@redhat.com>
    
    * don't remove CRDs when a components is removed
    
    Even after a component deletion, we want to keep the CRDs it brought. This
    should help us migrate components under different operators. If a user wants to
    remove CRDs too, it has to be done manually.
    
    Signed-off-by: Petr Horacek <phoracek@redhat.com>

--- pkg/controller/networkaddonsconfig/networkaddonsconfig_controller.go
+++ pkg/controller/networkaddonsconfig/networkaddonsconfig_controller.go
@@ -360,8 +360,12 @@ func (r *ReconcileNetworkAddonsConfig) renderObjectsToDelete(networkAddonsConfig
 // are removed when NetworkAddonsConfig config is
 func (r *ReconcileNetworkAddonsConfig) applyObjects(networkAddonsConfig *opv1alpha1.NetworkAddonsConfig, objs []*unstructured.Unstructured) error {
 	for _, obj := range objs {
-		// Mark the object to be GC'd if the owner is deleted. Don't set owner reference on namespaces if they are used by the operator itself
-		if !(obj.GetKind() == "Namespace" && obj.GetName() == operatorNamespace) {
+		// Mark the object to be GC'd if the owner is deleted.
+		// Don't set owner reference on namespaces if they are used by the operator itself
+		// Don't set owner reference on CRDs, they should survive removal of the operator
+		isOperatorNamespace := obj.GetKind() == "Namespace" && obj.GetName() == operatorNamespace
+		isCRD := obj.GetKind() == "CustomResourceDefinition"
+		if !isCRD && !isOperatorNamespace {
 			if err := controllerutil.SetControllerReference(networkAddonsConfig, obj, r.scheme); err != nil {
 				log.Printf("could not set reference for (%s) %s/%s: %v", obj.GroupVersionKind(), obj.GetNamespace(), obj.GetName(), err)
 				err = errors.Wrapf(err, "could not set reference for (%s) %s/%s", obj.GroupVersionKind(), obj.GetNamespace(), obj.GetName())
