f3c4ddb9aa9a826bb95f3b67fc1e3a2b07aa6510
Quique Llorente <ellorent@redhat.com>
2020-05-13 11:07:22 +0200

    Upgrade kubevirt provider to k8s-1.17
    Signed-off-by: Quique Llorente <ellorent@redhat.com>

--- automation/check-patch.e2e-workflow-k8s.sh
+++ automation/check-patch.e2e-workflow-k8s.sh
@@ -12,7 +12,7 @@ teardown() {
 }
 
 main() {
-    export KUBEVIRT_PROVIDER='k8s-1.17.0'
+    export KUBEVIRT_PROVIDER='k8s-1.17'
 
     source automation/check-patch.setup.sh
     cd ${TMP_PROJECT_PATH}
