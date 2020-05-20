4b58fd67f88abacd6bee2625e64cb4d59bb70436
Or Mergi <ormergi@redhat.com>
2020-05-20 18:30:41 +0300

    Post 'make components' file changes
    Signed-off-by: Or Mergi <ormergi@redhat.com>

--- test/releases/99.0.0.go
+++ test/releases/99.0.0.go
@@ -30,7 +30,7 @@ func init() {
 				ParentName: "kubemacpool-mac-controller-manager",
 				ParentKind: "Deployment",
 				Name:       "manager",
-				Image:      "quay.io/kubevirt/kubemacpool:v0.10.0",
+				Image:      "quay.io/kubevirt/kubemacpool:v0.12.0",
 			},
 			opv1alpha1.Container{
 				ParentName: "nmstate-handler",
