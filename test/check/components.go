e7e4a3ca84415a7baf154e2e13bb30492b72bb10
Enrique Llorente Pastora <ellorent@redhat.com>
2020-05-29 16:53:33 +0200

    Bump kubernetes-nmstate 0.21.0 (#403)
    Signed-off-by: Quique Llorente <ellorent@redhat.com>

--- test/check/components.go
+++ test/check/components.go
@@ -45,6 +45,9 @@ var (
 		DaemonSets: []string{
 			"nmstate-handler",
 		},
+		Deployments: []string{
+			"nmstate-webhook",
+		},
 	}
 	OvsComponent = Component{
 		ComponentName:              "Ovs",
