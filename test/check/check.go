c79fc1d2cdbcefee138d363a41d99a8f6dcfd1fb
Ram Lavi <ralavi@redhat.com>
2020-06-16 15:56:06 +0300

    Revert "status_manager: only require single ready pod for deployment"
    Currently, kubemacpool deployment is ready when at least 1 pod is ready.
    Due to changes braught up in kubemacpool v0.14.3, both kubemapool pods should be in status ready.
    Due to this, This commit reverts commit 2a053e585cfa430c8d1828a68ff25ef3aa446a6e.
    
    Signed-off-by: Ram Lavi <ralavi@redhat.com>

--- test/check/check.go
+++ test/check/check.go
@@ -339,7 +339,7 @@ func checkForDeployment(name string) error {
 		}
 	}
 
-	if deployment.Status.AvailableReplicas == 0 {
+	if deployment.Status.UnavailableReplicas > 0 || deployment.Status.AvailableReplicas == 0 {
 		manifest, err := yaml.Marshal(deployment)
 		if err != nil {
 			panic(err)
