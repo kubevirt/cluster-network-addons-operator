ed67626973d3b90624654f50b8a55dc8a3036251
Ram Lavi <ralavi@redhat.com>
2020-06-16 16:05:26 +0300

    bump kubemacpool to v0.14.3
    Signed-off-by: Ram Lavi <ralavi@redhat.com>

--- pkg/components/components.go
+++ pkg/components/components.go
@@ -23,7 +23,7 @@ const (
 	MultusImageDefault            = "nfvpe/multus:v3.4.1"
 	LinuxBridgeCniImageDefault    = "quay.io/kubevirt/cni-default-plugins:v0.8.6"
 	LinuxBridgeMarkerImageDefault = "quay.io/kubevirt/bridge-marker:0.3.0"
-	KubeMacPoolImageDefault       = "quay.io/kubevirt/kubemacpool:v0.14.2"
+	KubeMacPoolImageDefault       = "quay.io/kubevirt/kubemacpool:v0.14.3"
 	NMStateHandlerImageDefault    = "quay.io/nmstate/kubernetes-nmstate-handler:v0.21.0"
 	OvsCniImageDefault            = "quay.io/kubevirt/ovs-cni-plugin:v0.12.0"
 	OvsMarkerImageDefault         = "quay.io/kubevirt/ovs-cni-marker:v0.12.0"
