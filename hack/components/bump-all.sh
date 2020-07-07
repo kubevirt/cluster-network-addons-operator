e758e443d1be47da5ce5502b5b595f5a851fcbea
Ram Lavi <ralavi@redhat.com>
2020-07-07 15:40:03 +0300

    remove use of unecessary source cmd
    Signed-off-by: Ram Lavi <ralavi@redhat.com>

--- hack/components/bump-all.sh
+++ hack/components/bump-all.sh
@@ -2,18 +2,16 @@
 
 set -xeo pipefail
 
-CNAO_VERSION=${VERSION} # Exported from Makefile
-
 echo 'Setup temporary Go path'
 export GOPATH=${PWD}/_components/go
 mkdir -p $GOPATH
 export PATH=${GOPATH}/bin:${PATH}
 
 echo 'kubemacpool'
-source hack/components/bump-kubemacpool.sh
+CNAO_VERSION=${VERSION} ./hack/components/bump-kubemacpool.sh
 
 echo 'macvtap'
-source hack/components/bump-macvtap.sh
+CNAO_VERSION=${VERSION} ./hack/components/bump-macvtap.sh
 
 echo 'kubernetes-nmstate'
-source hack/components/bump-knmstate.sh
+CNAO_VERSION=${VERSION} ./hack/components/bump-knmstate.sh
