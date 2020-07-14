ad4c3a8a2fbe48b23465356854defd3425502c63
Petr Horacek <phoracek@redhat.com>
2020-07-14 17:07:05 +0200

    Release 0.39.3
    Signed-off-by: Petr Horacek <phoracek@redhat.com>

---
+++ test/releases/0.39.3.go
@@ -0,0 +1,66 @@
+package releases
+
+import (
+	opv1alpha1 "github.com/kubevirt/cluster-network-addons-operator/pkg/apis/networkaddonsoperator/v1alpha1"
+)
+
+func init() {
+	release := Release{
+		Version: "0.39.3",
+		Containers: []opv1alpha1.Container{
+			opv1alpha1.Container{
+				ParentName: "multus",
+				ParentKind: "DaemonSet",
+				Name:       "kube-multus",
+				Image:      "nfvpe/multus:v3.4.1",
+			},
+			opv1alpha1.Container{
+				ParentName: "bridge-marker",
+				ParentKind: "DaemonSet",
+				Name:       "bridge-marker",
+				Image:      "quay.io/kubevirt/bridge-marker:0.3.0",
+			},
+			opv1alpha1.Container{
+				ParentName: "kube-cni-linux-bridge-plugin",
+				ParentKind: "DaemonSet",
+				Name:       "cni-plugins",
+				Image:      "quay.io/kubevirt/cni-default-plugins:v0.8.6",
+			},
+			opv1alpha1.Container{
+				ParentName: "kubemacpool-mac-controller-manager",
+				ParentKind: "Deployment",
+				Name:       "manager",
+				Image:      "quay.io/kubevirt/kubemacpool:v0.14.5",
+			},
+			opv1alpha1.Container{
+				ParentName: "nmstate-handler",
+				ParentKind: "DaemonSet",
+				Name:       "nmstate-handler",
+				Image:      "quay.io/nmstate/kubernetes-nmstate-handler:v0.21.0",
+			},
+			opv1alpha1.Container{
+				ParentName: "ovs-cni-amd64",
+				ParentKind: "DaemonSet",
+				Name:       "ovs-cni-plugin",
+				Image:      "quay.io/kubevirt/ovs-cni-plugin:v0.12.0",
+			},
+			opv1alpha1.Container{
+				ParentName: "ovs-cni-amd64",
+				ParentKind: "DaemonSet",
+				Name:       "ovs-cni-marker",
+				Image:      "quay.io/kubevirt/ovs-cni-marker:v0.12.0",
+			},
+		},
+		SupportedSpec: opv1alpha1.NetworkAddonsConfigSpec{
+			KubeMacPool: &opv1alpha1.KubeMacPool{},
+			LinuxBridge: &opv1alpha1.LinuxBridge{},
+			Multus:      &opv1alpha1.Multus{},
+			NMState:     &opv1alpha1.NMState{},
+			Ovs:         &opv1alpha1.Ovs{},
+		},
+		Manifests: []string{
+			"operator.yaml",
+		},
+	}
+	releases = append(releases, release)
+}
