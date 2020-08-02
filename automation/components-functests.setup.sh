e59f656c2f334c546456fac58ac5a65032b26f08
Or Mergi <ormergi@redhat.com>
2020-08-02 18:28:04 +0300

    automation: add components-functests.setup  script
    It is necessary to put all common steps for components
    (e.g: KubeMacPool, NMstate) e2e-functests on CNAO
    in one place.
    This commit adds
    automation/componenets-functests.setup.sh
    script to address that.
    
    What this script does:
    - Spin up k8s ephemeral cluster
    - Deploys latest changes CNAO
    - Clones a component (e.g: KubeMacPool)
      latest commit repository.
    
    This script also exports .kubeconfig file path
    to the env variable 'KUBECONFIG' of the created
    cluster.
    
    Signed-off-by: Or Mergi <ormergi@redhat.com>

---
+++ automation/components-functests.setup.sh
@@ -0,0 +1,73 @@
+#! /bin/bash
+set -exu
+
+# Create an ephemeral cluster with latest changes
+# CNAO and clones the desired component repository
+#
+# This script exports:
+# - KUBECONFIG
+#   kubeconfig full path so you can use`kubectl` binary or
+#   CNAO's `./cluster/kubectl.sh` script directly.
+#
+# - TMP_COMPONENT_PATH
+#   The desired component temp directory
+#
+# Example:
+# COMPONENT="kubemacpool" source automation/e2e-functest.setup.sh
+# cd ${TMP_COMPONENT_PATH}
+# KUBECONFIG=$KUBECONFIG make functests
+
+source hack/components/git-utils.sh
+source hack/components/yaml-utils.sh
+source cluster/kubevirtci.sh
+
+teardown() {
+    make cluster-down
+    rm -rf "$component_temp_dir"
+}
+
+trap teardown EXIT SIGINT SIGTERM SIGQUIT
+
+# Spin up Kubernetes cluster
+export KUBEVIRT_PROVIDER='k8s-1.17'
+make cluster-down cluster-up
+
+# Export .kubeconfig full path, so it will be possible
+# to use 'kubectl' directly from the component directory path
+export KUBECONFIG=$(kubevirtci::kubeconfig)
+
+# Deploy CNAO latest changes
+make cluster-operator-push
+make cluster-operator-install
+
+# Deploy all network addons components with CNAO
+    cat <<EOF | kubectl apply -f -
+apiVersion: networkaddonsoperator.network.kubevirt.io/v1alpha1
+kind: NetworkAddonsConfig
+metadata:
+  name: cluster
+spec:
+  multus: {}
+  linuxBridge: {}
+  kubeMacPool:
+   rangeStart: "02:00:00:00:00:00"
+   rangeEnd: "02:00:00:00:00:0F"
+  nmstate: {}
+  ovs: {}
+  macvtap: {}
+  imagePullPolicy: Always
+EOF
+
+kubectl wait networkaddonsconfig cluster --for condition=Available --timeout=13m
+
+# Clone component repository
+component_url=$(yaml-utils::get_component_url ${COMPONENT})
+component_commit=$(yaml-utils::get_component_commit ${COMPONENT})
+component_repo=$(yaml-utils::get_component_repo ${component_url})
+
+component_temp_dir=$(git-utils::create_temp_path ${COMPONENT})
+component_path=${component_temp_dir}/${component_repo}
+
+git-utils::fetch_component ${component_path} ${component_url} ${component_commit}
+
+export TMP_COMPONENT_PATH=${component_path}
