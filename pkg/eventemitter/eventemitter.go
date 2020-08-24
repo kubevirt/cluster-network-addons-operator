69db781fd004309a5dba109350fededa8964ac7c
alonSadan <asadan@redhat.com>
2020-08-24 21:45:40 +0300

    utils, events: add events-emitter module to emit k8s events
    In order to bring CNAO to meet operators conventional best practices,
    It should use k8s events to describe actions that are being taken.
    In this commit a new event emitter is introduced, to easily produce events.
    the emitter has to be initiated and then can be used to create events.
    
    Right now the new emitter is aimed to be used only to emit events for configs,
    but can be improved in the future to also support associating events to
    CSVs and the self pod.
    
    Signed-off-by: alonSadan <asadan@redhat.com>

---
+++ pkg/eventemitter/eventemitter.go
@@ -0,0 +1,97 @@
+package eventemitter
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/client-go/tools/record"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/manager"
+
+	cnaov1 "github.com/kubevirt/cluster-network-addons-operator/pkg/apis/networkaddonsoperator/v1"
+	"github.com/kubevirt/cluster-network-addons-operator/pkg/names"
+)
+
+const (
+	ModifiedMessage    = "Config spec was modified"
+	ProgressingMessage = "Operator is progressing"
+	AvailableMessage   = "All applied componenets are available"
+	FailedMessage      = "Operator failed"
+)
+
+const (
+	AvailableReason   = "Available"
+	ProgressingReason = "Progressing"
+	FailedReason      = "Failed"
+	ModifiedReason    = "Modified"
+)
+
+type EventEmitter interface {
+	Init(mgr manager.Manager)
+	EmitEvent(object runtime.Object, eventType, reason, msg string)
+	EmitModifiedForConfig()
+	EmitProgressingForConfig()
+	EmitFailingForConfig(reason, message string)
+	EmitAvailableForConfig()
+}
+
+type eventEmitter struct {
+	recorder record.EventRecorder
+	client   client.Client
+}
+
+// New event emitter
+func New(mgr manager.Manager) EventEmitter {
+	var evntEmtr EventEmitter = &eventEmitter{
+		client: mgr.GetClient(),
+	}
+
+	evntEmtr.Init(mgr)
+	return evntEmtr
+}
+
+func (ee *eventEmitter) Init(mgr manager.Manager) {
+	ee.recorder = mgr.GetEventRecorderFor(names.OPERATOR_CONFIG)
+}
+
+func (ee eventEmitter) EmitEvent(object runtime.Object, eventType, reason, msg string) {
+	if object != nil {
+		ee.recorder.Event(object, eventType, reason, msg)
+	}
+}
+
+func (ee eventEmitter) EmitProgressingForConfig() {
+	config := ee.getConfigForEmitter()
+	ee.EmitEvent(config, corev1.EventTypeNormal, ProgressingReason, ProgressingMessage)
+}
+
+func (ee eventEmitter) EmitFailingForConfig(reason, message string) {
+	config := ee.getConfigForEmitter()
+	ee.EmitEvent(config, corev1.EventTypeWarning, fmt.Sprintf("%s: %s", FailedReason, reason), fmt.Sprintf("%s: %s", FailedMessage, message))
+}
+
+func (ee eventEmitter) EmitAvailableForConfig() {
+	config := ee.getConfigForEmitter()
+	ee.EmitEvent(config, corev1.EventTypeNormal, AvailableReason, AvailableMessage)
+}
+
+func (ee eventEmitter) EmitModifiedForConfig() {
+	config := ee.getConfigForEmitter()
+	ee.EmitEvent(config, corev1.EventTypeNormal, ModifiedReason, ModifiedMessage)
+}
+
+func (ee eventEmitter) getConfigForEmitter() *cnaov1.NetworkAddonsConfig {
+	config := &cnaov1.NetworkAddonsConfig{}
+	err := ee.client.Get(context.TODO(), types.NamespacedName{Name: names.OPERATOR_CONFIG}, config)
+	if err != nil {
+		log.Printf("Failed to get NetworkAddonsConfig in order emit event. %v", err)
+
+		return nil
+	}
+
+	return config
+}
