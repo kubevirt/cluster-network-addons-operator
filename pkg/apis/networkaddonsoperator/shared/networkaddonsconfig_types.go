67e4e2e8297b5ebf4e2d7f51bf0501b2018e5897
Ram Lavi <ralavi@redhat.com>
2020-08-24 15:33:33 +0300

    update empty v1 and v1alpha1's API-Version types to use shared types
    We want to reuse shared API-Version type structs to be used both on v1alpha1 and v1.
    This way both API-Versions use the same structs via aliasing
    
    Signed-off-by: Ram Lavi <ralavi@redhat.com>

--- pkg/apis/networkaddonsoperator/shared/networkaddonsconfig_types.go
+++ pkg/apis/networkaddonsoperator/shared/networkaddonsconfig_types.go
@@ -9,13 +9,13 @@ import (
 // NetworkAddonsConfigSpec defines the desired state of NetworkAddonsConfig
 // +k8s:openapi-gen=true
 type NetworkAddonsConfigSpec struct {
-	Multus           *Multus           `json:"multus,omitempty"`
-	LinuxBridge      *LinuxBridge      `json:"linuxBridge,omitempty"`
-	Ovs              *Ovs              `json:"ovs,omitempty"`
-	KubeMacPool      *KubeMacPool      `json:"kubeMacPool,omitempty"`
-	ImagePullPolicy  corev1.PullPolicy `json:"imagePullPolicy,omitempty"`
-	NMState          *NMState          `json:"nmstate,omitempty"`
-	MacvtapCni       *MacvtapCni       `json:"macvtap,omitempty"`
+	Multus                *Multus                `json:"multus,omitempty"`
+	LinuxBridge           *LinuxBridge           `json:"linuxBridge,omitempty"`
+	Ovs                   *Ovs                   `json:"ovs,omitempty"`
+	KubeMacPool           *KubeMacPool           `json:"kubeMacPool,omitempty"`
+	ImagePullPolicy       corev1.PullPolicy      `json:"imagePullPolicy,omitempty"`
+	NMState               *NMState               `json:"nmstate,omitempty"`
+	MacvtapCni            *MacvtapCni            `json:"macvtap,omitempty"`
 	SelfSignConfiguration *SelfSignConfiguration `json:"selfSignConfiguration,omitempty"`
 }
 
--- pkg/apis/networkaddonsoperator/v1/networkaddonsconfig_types.go
+++ pkg/apis/networkaddonsoperator/v1/networkaddonsconfig_types.go
@@ -2,36 +2,22 @@ package v1
 
 import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	"github.com/kubevirt/cluster-network-addons-operator/pkg/apis/networkaddonsoperator/shared"
 )
-
-// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
-// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
-
-// NetworkAddonsConfigSpec defines the desired state of NetworkAddonsConfig
-type NetworkAddonsConfigSpec struct {
-	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
-	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
-	// Add custom validation using kubebuilder tags: https://book-v1.book.kubebuilder.io/beyond_basics/generating_crd.html
-}
-
-// NetworkAddonsConfigStatus defines the observed state of NetworkAddonsConfig
-type NetworkAddonsConfigStatus struct {
-	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
-	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
-	// Add custom validation using kubebuilder tags: https://book-v1.book.kubebuilder.io/beyond_basics/generating_crd.html
-}
 
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 
 // NetworkAddonsConfig is the Schema for the networkaddonsconfigs API
 // +kubebuilder:subresource:status
 // +kubebuilder:resource:path=networkaddonsconfigs,scope=Namespaced
+// +k8s:openapi-gen=true
 type NetworkAddonsConfig struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
 
-	Spec   NetworkAddonsConfigSpec   `json:"spec,omitempty"`
-	Status NetworkAddonsConfigStatus `json:"status,omitempty"`
+	Spec   shared.NetworkAddonsConfigSpec   `json:"spec,omitempty"`
+	Status shared.NetworkAddonsConfigStatus `json:"status,omitempty"`
 }
 
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
