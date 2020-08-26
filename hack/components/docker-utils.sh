d3a856cd0b561da7862a785a435c9ba7f9da93fd
Nahshon Unna Tsameret <60659093+nunnatsa@users.noreply.github.com>
2020-08-26 17:06:03 +0300

    Use image digests instead of tags (#518)
    * Use image digests instead of tags
    
    In order to enable deployment in disconnected environment, all the images in the CSV must be with digests instead of tag.
    
    Signed-off-by: Nahshon Unna-Tsameret <nunnatsa@redhat.com>
    
    * fix code review comments
    
    Signed-off-by: Nahshon Unna-Tsameret <nunnatsa@redhat.com>

---
+++ hack/components/docker-utils.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+set -xo pipefail
+
+# The get_image_digest function retrieves the image digest from the registry, according to the
+# image name (registry:tag format).
+#
+# Parameters:
+# 1. image name in registry:tag format
+# 2. image name without the tag
+#
+# Returns
+# The image digest
+#
+function docker-utils::get_image_digest() {
+  echo "${2}@$(docker run --rm quay.io/skopeo/stable:latest inspect "docker://${1}" | jq -r '.Digest')"
+}
+
+# The check_image_exists function checks if an image already exists in the registry.
+#
+# Parameters:
+# 1. image name in registry:tag format
+# 2. image tag
+#
+# returns the image tag if found; else, returns an empty result
+#
+function docker-utils::check_image_exists() {
+  docker run --rm quay.io/skopeo/stable:latest list-tags "docker://${1}" | grep "\"${2}\""
+}
--- test/releases/99.0.0.go
+++ test/releases/99.0.0.go
@@ -8,53 +8,53 @@ func init() {
 	release := Release{
 		Version: "99.0.0",
 		Containers: []opv1alpha1.Container{
-			opv1alpha1.Container{
+			{
 				ParentName: "multus",
 				ParentKind: "DaemonSet",
 				Name:       "kube-multus",
-				Image:      "nfvpe/multus:v3.4.1",
+				Image:      "nfvpe/multus@sha256:167722b954355361bd69829466f27172b871dbdbf86b85a95816362885dc0aba",
 			},
-			opv1alpha1.Container{
+			{
 				ParentName: "bridge-marker",
 				ParentKind: "DaemonSet",
 				Name:       "bridge-marker",
-				Image:      "quay.io/kubevirt/bridge-marker:0.3.0",
+				Image:      "quay.io/kubevirt/bridge-marker@sha256:e55f73526468fee46a35ae41aa860f492d208b8a7a132832c5b9a76d4a51566a",
 			},
-			opv1alpha1.Container{
+			{
 				ParentName: "kube-cni-linux-bridge-plugin",
 				ParentKind: "DaemonSet",
 				Name:       "cni-plugins",
-				Image:      "quay.io/kubevirt/cni-default-plugins:v0.8.6",
+				Image:      "quay.io/kubevirt/cni-default-plugins@sha256:680ac8fd5eeab39c9a3c01479da344bdcaa43aa065d07ae00513b7bafa22fccf",
 			},
-			opv1alpha1.Container{
+			{
 				ParentName: "kubemacpool-mac-controller-manager",
 				ParentKind: "Deployment",
 				Name:       "manager",
-				Image:      "quay.io/kubevirt/kubemacpool:v0.19.0",
+				Image:      "quay.io/kubevirt/kubemacpool@sha256:34c470f7654da6d4407cc9bcb79c871cd5c90a3396861dc2ff64ad013ef0aa74",
 			},
-			opv1alpha1.Container{
+			{
 				ParentName: "nmstate-handler",
 				ParentKind: "DaemonSet",
 				Name:       "nmstate-handler",
-				Image:      "quay.io/nmstate/kubernetes-nmstate-handler:v0.29.0",
+				Image:      "quay.io/nmstate/kubernetes-nmstate-handler@sha256:76cc13fb4a60943dca6038619599b6a49fe451852aba23ad3046658429a9af30",
 			},
-			opv1alpha1.Container{
+			{
 				ParentName: "nmstate-webhook",
 				ParentKind: "Deployment",
 				Name:       "nmstate-webhook",
-				Image:      "quay.io/nmstate/kubernetes-nmstate-handler:v0.29.0",
+				Image:      "quay.io/nmstate/kubernetes-nmstate-handler@sha256:76cc13fb4a60943dca6038619599b6a49fe451852aba23ad3046658429a9af30",
 			},
-			opv1alpha1.Container{
+			{
 				ParentName: "ovs-cni-amd64",
 				ParentKind: "DaemonSet",
 				Name:       "ovs-cni-plugin",
-				Image:      "quay.io/kubevirt/ovs-cni-plugin:v0.12.0",
+				Image:      "quay.io/kubevirt/ovs-cni-plugin@sha256:4101c52617efb54a45181548c257a08e3689f634b79b9dfcff42bffd8b25af53",
 			},
-			opv1alpha1.Container{
+			{
 				ParentName: "ovs-cni-amd64",
 				ParentKind: "DaemonSet",
 				Name:       "ovs-cni-marker",
-				Image:      "quay.io/kubevirt/ovs-cni-marker:v0.12.0",
+				Image:      "quay.io/kubevirt/ovs-cni-marker@sha256:0f08d6b1550a90c9f10221f2bb07709d1090e7c675ee1a711981bd429074d620",
 			},
 		},
 		SupportedSpec: opv1alpha1.NetworkAddonsConfigSpec{
