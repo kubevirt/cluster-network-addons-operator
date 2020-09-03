62e9a06e93ea86b2b6143e252d8acfb879bf9f46
Ram Lavi <ralavi@redhat.com>
2020-09-03 16:48:39 +0300

    add PlacementConfiguration for multus
    Use Infra PlacementConfiguration from NetworkAddonsConfig in multus
    By default, multus will be scheduled on master nodes.
    
    Signed-off-by: Ram Lavi <ralavi@redhat.com>

--- hack/components/bump-multus.sh
+++ hack/components/bump-multus.sh
@@ -29,6 +29,9 @@ function __parametize_by_object() {
 				yaml-utils::update_param ${f} spec.template.spec.volumes[0].hostPath.path '{{ .CNIConfigDir }}'
 				yaml-utils::update_param ${f} spec.template.spec.volumes[1].hostPath.path '{{ .CNIBinDir }}'
 				yaml-utils::delete_param ${f} spec.template.spec.volumes[2]
+				yaml-utils::update_param ${f} spec.template.spec.nodeSelector '{{ toYaml .Placement.NodeSelector | nindent 8 }}'
+				yaml-utils::set_param ${f} spec.template.spec.affinity '{{ toYaml .Placement.Affinity | nindent 8 }}'
+				yaml-utils::update_param ${f} spec.template.spec.tolerations '{{ toYaml .Placement.Tolerations | nindent 8 }}'
 				yaml-utils::remove_single_quotes_from_yaml ${f}
 				;;
 		esac
