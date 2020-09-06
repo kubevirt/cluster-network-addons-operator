afe2027ad21cf161030388cc6e079d9f655fbffa
alonSadan <asadan@redhat.com>
2020-09-06 15:38:08 +0300

    tests, events: add tests framework for event emitter
    - Added a new module for watchhing config Events. This is almost Identical
      to the framwork from kubevirt/kubevort/tests/utils.go
    - added checks for all types of config events, to verify they were emitted.
      Can be used in tests, to verify deployment status.
    
    Signed-off-by: alonSadan <asadan@redhat.com>

---
+++ test/check/events.go
@@ -0,0 +1,211 @@
+package check
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"reflect"
+	"time"
+
+	framework "github.com/operator-framework/operator-sdk/pkg/test"
+	corev1 "k8s.io/api/core/v1"
+
+	"k8s.io/apimachinery/pkg/api/meta"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/fields"
+	"k8s.io/apimachinery/pkg/runtime"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+type EventType string
+
+const (
+	NormalEvent  EventType = "Normal"
+	WarningEvent EventType = "Warning"
+)
+
+type startType string
+
+const (
+	invalidWatch startType = "invalidWatch"
+	// Watch since the moment a long poll connection is established
+	watchSinceNow startType = "watchSinceNow"
+	// Watch since the resourceVersion of the passed in runtime object
+	watchSinceObjectUpdate startType = "watchSinceObjectUpdate"
+	// Watch since the resourceVersion of the watched object
+	watchSinceWatchedObjectUpdate startType = "watchSinceWatchedObjectUpdate"
+	// Watch since the resourceVersion passed in to the builder
+	watchSinceResourceVersion startType = "watchSinceResourceVersion"
+)
+
+type ProcessFunc func(event *corev1.Event) (done bool)
+
+type ObjectEventWatcher struct {
+	object                 runtime.Object
+	timeout                *time.Duration
+	failOnWarnings         bool
+	resourceVersion        string
+	startType              startType
+	dontFailOnMissingEvent bool
+}
+
+func NewObjectEventWatcher(object runtime.Object) *ObjectEventWatcher {
+	return &ObjectEventWatcher{object: object, startType: invalidWatch}
+}
+
+func (w *ObjectEventWatcher) Timeout(duration time.Duration) *ObjectEventWatcher {
+	w.timeout = &duration
+	return w
+}
+
+func (w *ObjectEventWatcher) FailOnWarnings() *ObjectEventWatcher {
+	w.failOnWarnings = true
+	return w
+}
+
+/*
+SinceNow sets a watch starting point for events, from the moment on the connection to the apiserver
+was established.
+*/
+func (w *ObjectEventWatcher) SinceNow() *ObjectEventWatcher {
+	w.startType = watchSinceNow
+	return w
+}
+
+/*
+SinceWatchedObjectResourceVersion takes the resource version of the runtime object which is watched,
+and takes it as the starting point for all events to watch for.
+*/
+func (w *ObjectEventWatcher) SinceWatchedObjectResourceVersion() *ObjectEventWatcher {
+	w.startType = watchSinceWatchedObjectUpdate
+	return w
+}
+
+/*
+SinceObjectResourceVersion takes the resource version of the passed in runtime object and takes it
+as the starting point for all events to watch for.
+*/
+func (w *ObjectEventWatcher) SinceObjectResourceVersion(object runtime.Object) *ObjectEventWatcher {
+	var err error
+	w.startType = watchSinceObjectUpdate
+	w.resourceVersion, err = meta.NewAccessor().ResourceVersion(object)
+	Expect(err).ToNot(HaveOccurred())
+	return w
+}
+
+/*
+SinceResourceVersion sets the passed in resourceVersion as the starting point for all events to watch for.
+*/
+func (w *ObjectEventWatcher) SinceResourceVersion(rv string) *ObjectEventWatcher {
+	w.resourceVersion = rv
+	w.startType = watchSinceResourceVersion
+	return w
+}
+
+func (w *ObjectEventWatcher) Watch(abortChan chan struct{}, processFunc ProcessFunc, watchedDescription string) {
+	Expect(w.startType).ToNot(Equal(invalidWatch))
+	resourceVersion := ""
+
+	switch w.startType {
+	case watchSinceNow:
+		resourceVersion = ""
+	case watchSinceObjectUpdate, watchSinceResourceVersion:
+		resourceVersion = w.resourceVersion
+	case watchSinceWatchedObjectUpdate:
+		var err error
+		resourceVersion, err = meta.NewAccessor().ResourceVersion(w.object)
+		Expect(err).ToNot(HaveOccurred())
+	}
+
+	cli := framework.Global.KubeClient
+
+	f := processFunc
+
+	if w.failOnWarnings {
+		f = func(event *corev1.Event) bool {
+			msg := fmt.Sprintf("Event(%#v): type: '%v' reason: '%v' %v", event.InvolvedObject, event.Type, event.Reason, event.Message)
+			if event.Type == string(WarningEvent) {
+				log.Printf("unexpected warning event received: %s", msg)
+			} else {
+				log.Printf("%s", msg)
+			}
+			ExpectWithOffset(1, event.Type).NotTo(Equal(string(WarningEvent)), "Unexpected Warning event received: %s,%s: %s", event.InvolvedObject.Name, event.InvolvedObject.UID, event.Message)
+			return processFunc(event)
+		}
+
+	} else {
+		f = func(event *corev1.Event) bool {
+			msg := fmt.Sprintf("Event(%#v): type: '%v' reason: '%v' %v", event.InvolvedObject, event.Type, event.Reason, event.Message)
+			if event.Type == string(WarningEvent) {
+				log.Printf("Warning event received: %s", msg)
+			} else {
+				log.Printf("%s", msg)
+			}
+			return processFunc(event)
+		}
+	}
+
+	uid := w.object.(metav1.ObjectMetaAccessor).GetObjectMeta().GetName()
+	eventWatcher, err := cli.CoreV1().Events(corev1.NamespaceAll).
+		Watch(context.Background(), metav1.ListOptions{
+			FieldSelector:   fields.ParseSelectorOrDie("involvedObject.name=" + string(uid)).String(),
+			ResourceVersion: resourceVersion,
+		})
+	if err != nil {
+		panic(err)
+	}
+	defer eventWatcher.Stop()
+	done := make(chan struct{})
+	// iterate over the events and assert
+	go func() {
+		defer GinkgoRecover()
+		for obj := range eventWatcher.ResultChan() {
+			if f(obj.Object.(*corev1.Event)) {
+				close(done)
+				break
+			}
+		}
+	}()
+	// blocks until one of the channels receive
+	if w.timeout != nil {
+		select {
+		case <-done:
+		case <-abortChan:
+		case <-time.After(*w.timeout):
+			if !w.dontFailOnMissingEvent {
+				Fail(fmt.Sprintf("Waited for %v seconds on the event stream to match a specific event: %s", w.timeout.Seconds(), watchedDescription), 1)
+			}
+		}
+	} else {
+		select {
+		case <-abortChan:
+		case <-done:
+		}
+	}
+}
+
+func (w *ObjectEventWatcher) WaitFor(stopChan chan struct{}, eventType EventType, reason interface{}) (e *corev1.Event) {
+	w.Watch(stopChan, func(event *corev1.Event) bool {
+		if event.Type == string(eventType) && event.Reason == reflect.ValueOf(reason).String() {
+			e = event
+			return true
+		}
+		return false
+	}, fmt.Sprintf("event type %s, reason = %s", string(eventType), reflect.ValueOf(reason).String()))
+	return
+}
+
+func (w *ObjectEventWatcher) WaitNotFor(stopChan chan struct{}, eventType EventType, reason interface{}) (e *corev1.Event) {
+	w.dontFailOnMissingEvent = true
+	w.Watch(stopChan, func(event *corev1.Event) bool {
+		if event.Type == string(eventType) && event.Reason == reflect.ValueOf(reason).String() {
+			e = event
+			Fail(fmt.Sprintf("Did not expect %s with reason %s", string(eventType), reflect.ValueOf(reason).String()), 1)
+			return true
+		}
+		return false
+	}, fmt.Sprintf("not happen event type %s, reason = %s", string(eventType), reflect.ValueOf(reason).String()))
+	return
+}
