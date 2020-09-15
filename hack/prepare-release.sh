bde1ba2131ac5100a78563f41dc602cf3d158af6
Radim Hrazdil <rhrazdil@redhat.com>
2020-09-15 11:04:00 +0200

    Fix prepare-release.sh to use prefixed IMAGE_TAG
    Pass prefixed_released_version as IMAGE_TAG so that CNAO image URLs have format:
    "quay.io/kubevirt/cluster-network-addons-operator:v(major.minor.patch)".
    Also Updated sed commands to include the 'v' prefix in README.
    
    Signed-off-by: Radim Hrazdil <rhrazdil@redhat.com>

--- hack/prepare-release.sh
+++ hack/prepare-release.sh
@@ -5,15 +5,17 @@ set -ex
 version_type=$1
 previous_version=$(hack/version.sh)
 released_version=$(hack/bump-version.sh $version_type)
-commits=$(git log --pretty=format:"* %s" $previous_version..HEAD)
+prefixed_previous_version="v${previous_version}"
+prefixed_released_version="v${released_version}"
+commits=$(git log --pretty=format:"* %s" $prefixed_previous_version..HEAD)
 
 echo 'Build manifests for the new release'
-VERSION=${released_version} IMAGE_TAG=${released_version} make gen-manifests
+VERSION=${released_version} IMAGE_TAG=${prefixed_released_version} make gen-manifests
 git add manifests/cluster-network-addons/${released_version}
 
 echo 'Upgrade README.md with the released manifests'
-sed -i "s/\(.*kubectl apply.*\)${previous_version}\(.*\)/\1${released_version}\2/g" README.md
-sed -i "s/\(.*startingCSV.*\)${previous_version}\(.*\)/\1${released_version}\2/g" README.md
+sed -i "s/\(.*kubectl apply.*\)${prefixed_previous_version}\(.*\)/\1${prefixed_released_version}\2/g" README.md
+sed -i "s/\(.*startingCSV.*\)${prefixed_previous_version}\(.*\)/\1${prefixed_released_version}\2/g" README.md
 
 echo 'Generating new release for workflow e2e tests'
 cp test/releases/99.0.0.go test/releases/${released_version}.go
