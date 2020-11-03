fa1df96d89d5c6fad893209dad371683d7f80781
Ram Lavi <ralavi@redhat.com>
2020-11-03 10:12:35 +0200

    placement-configuration, Allow day2 changes
    Currently placement-configuration should not allow
    day2 changes.
    Changed to allow day2 changes
    
    Signed-off-by: Ram Lavi <ralavi@redhat.com>

--- pkg/network/placement_configuration.go
+++ pkg/network/placement_configuration.go
@@ -1,16 +1,13 @@
 package network
 
 import (
-	"reflect"
-
-	"github.com/pkg/errors"
 	corev1 "k8s.io/api/core/v1"
 
 	cnao "github.com/kubevirt/cluster-network-addons-operator/pkg/apis/networkaddonsoperator/shared"
 )
 
-var (
-	defaultPlacementConfiguration = cnao.PlacementConfiguration{
+func GetDefaultPlacementConfiguration() cnao.PlacementConfiguration {
+	return cnao.PlacementConfiguration{
 		Infra: &cnao.Placement{
 			NodeSelector: map[string]string{
 				"beta.kubernetes.io/arch":        "amd64",
@@ -36,32 +33,19 @@ var (
 			},
 		},
 	}
-)
-
-func fillDefaultsPlacementConfiguration(conf, previous *cnao.NetworkAddonsConfigSpec) []error {
-	if conf.PlacementConfiguration == nil || conf.PlacementConfiguration.Infra == nil || conf.PlacementConfiguration.Workloads == nil {
-		if previous != nil && previous.PlacementConfiguration != nil {
-			conf.PlacementConfiguration = previous.PlacementConfiguration
-			return []error{}
-		}
-
-		if conf.PlacementConfiguration == nil {
-			conf.PlacementConfiguration = &defaultPlacementConfiguration
-			return []error{}
-		}
-		if conf.PlacementConfiguration.Infra == nil {
-			conf.PlacementConfiguration.Infra = defaultPlacementConfiguration.Infra
-		}
-		if conf.PlacementConfiguration.Workloads == nil {
-			conf.PlacementConfiguration.Workloads = defaultPlacementConfiguration.Workloads
-		}
-	}
-	return []error{}
 }
 
-func changeSafePlacementConfiguration(prev, next *cnao.NetworkAddonsConfigSpec) []error {
-	if prev.PlacementConfiguration != nil && next.PlacementConfiguration != nil && !reflect.DeepEqual(prev.PlacementConfiguration, next.PlacementConfiguration) {
-		return []error{errors.Errorf("cannot modify PlacementConfiguration once it is deployed")}
+func fillDefaultsPlacementConfiguration(conf, previous *cnao.NetworkAddonsConfigSpec) []error {
+	defaultPlacementConfiguration := GetDefaultPlacementConfiguration()
+	if conf.PlacementConfiguration == nil {
+		conf.PlacementConfiguration = &defaultPlacementConfiguration
+		return []error{}
+	}
+	if conf.PlacementConfiguration.Infra == nil {
+		conf.PlacementConfiguration.Infra = defaultPlacementConfiguration.Infra
+	}
+	if conf.PlacementConfiguration.Workloads == nil {
+		conf.PlacementConfiguration.Workloads = defaultPlacementConfiguration.Workloads
 	}
 	return []error{}
 }
