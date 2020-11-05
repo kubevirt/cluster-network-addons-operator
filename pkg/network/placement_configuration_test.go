7fbeb617b233d301896587c6ac6a43e6a69e2e7f
RamLavi <48101085+RamLavi@users.noreply.github.com>
2020-11-05 12:47:51 +0200

    placement-configuration, Allow day2 changes (#639)
    Currently placement-configuration should not allow
    day2 changes.
    Changed to allow day2 changes
    
    Signed-off-by: Ram Lavi <ralavi@redhat.com>

--- pkg/network/placement_configuration_test.go
+++ pkg/network/placement_configuration_test.go
@@ -14,6 +14,7 @@ var _ = Describe("Testing PlacementConfiguration", func() {
 		currentConfig  *cnao.NetworkAddonsConfigSpec
 		expectedConfig cnao.PlacementConfiguration
 	}
+	defaultPlacementConfiguration := GetDefaultPlacementConfiguration()
 	DescribeTable("Fill defaults function",
 		func(c fillDefaultsCase) {
 			errorList := fillDefaultsPlacementConfiguration(c.currentConfig, c.previousConfig)
@@ -49,95 +50,34 @@ var _ = Describe("Testing PlacementConfiguration", func() {
 				Infra:     defaultPlacementConfiguration.Infra,
 			},
 		}),
-		Entry("When the user hasn't explicitly changed PlacementConfiguration and a previous configuration exits should use the previous values on the current one", fillDefaultsCase{
-			previousConfig: &cnao.NetworkAddonsConfigSpec{
+		Entry("When new Infra & Workloads PlacementConfiguration is not nil should keep new values", fillDefaultsCase{
+			previousConfig: &cnao.NetworkAddonsConfigSpec{},
+			currentConfig: &cnao.NetworkAddonsConfigSpec{
 				PlacementConfiguration: &cnao.PlacementConfiguration{
 					Workloads: &cnao.Placement{
 						NodeSelector: map[string]string{
 							"beta.kubernetes.io/arch": "amd64",
 						},
 					},
+					Infra: &cnao.Placement{
+						NodeSelector: map[string]string{
+							"node-role.kubernetes.io/master": "",
+						},
+					},
 				},
 			},
-			currentConfig: &cnao.NetworkAddonsConfigSpec{},
 			expectedConfig: cnao.PlacementConfiguration{
 				Workloads: &cnao.Placement{
 					NodeSelector: map[string]string{
 						"beta.kubernetes.io/arch": "amd64",
 					},
 				},
-			},
-		}),
-	)
-	type changeSafeCase struct {
-		previousConfig *cnao.NetworkAddonsConfigSpec
-		currentConfig  *cnao.NetworkAddonsConfigSpec
-		expectedError  string
-	}
-	DescribeTable("Change safe function",
-		func(c changeSafeCase) {
-			errorList := changeSafePlacementConfiguration(c.previousConfig, c.currentConfig)
-			if c.expectedError == "" {
-				Expect(errorList).To(BeEmpty())
-			} else {
-				Expect(len(errorList)).To(Equal(1), "validation failed due to an unexpected error: %v", errorList)
-				Expect(errorList[0].Error()).To(MatchRegexp(c.expectedError))
-			}
-		},
-		Entry("When they are equal should NOT return an error", changeSafeCase{
-			previousConfig: &cnao.NetworkAddonsConfigSpec{
-				PlacementConfiguration: &cnao.PlacementConfiguration{
-					Infra: &cnao.Placement{
-						NodeSelector: map[string]string{
-							"label": "value",
-						},
+				Infra: &cnao.Placement{
+					NodeSelector: map[string]string{
+						"node-role.kubernetes.io/master": "",
 					},
 				},
 			},
-			currentConfig: &cnao.NetworkAddonsConfigSpec{
-				PlacementConfiguration: &cnao.PlacementConfiguration{
-					Infra: &cnao.Placement{
-						NodeSelector: map[string]string{
-							"label": "value",
-						},
-					},
-				},
-			},
-			expectedError: "",
-		}),
-		Entry("When they are not equal should return an error", changeSafeCase{
-			previousConfig: &cnao.NetworkAddonsConfigSpec{
-				PlacementConfiguration: &cnao.PlacementConfiguration{
-					Infra: &cnao.Placement{
-						NodeSelector: map[string]string{
-							"label": "value",
-						},
-					},
-				},
-			},
-			currentConfig: &cnao.NetworkAddonsConfigSpec{
-				PlacementConfiguration: &cnao.PlacementConfiguration{
-					Infra: &cnao.Placement{
-						NodeSelector: map[string]string{
-							"newlabel": "newvalue",
-						},
-					},
-				},
-			},
-			expectedError: "cannot modify PlacementConfiguration once it is deployed",
-		}),
-		Entry("When trying to remove PlacementConfiguration should NOT return an error", changeSafeCase{
-			previousConfig: &cnao.NetworkAddonsConfigSpec{
-				PlacementConfiguration: &cnao.PlacementConfiguration{
-					Infra: &cnao.Placement{
-						NodeSelector: map[string]string{
-							"label": "value",
-						},
-					},
-				},
-			},
-			currentConfig: &cnao.NetworkAddonsConfigSpec{},
-			expectedError: "",
 		}),
 	)
 })
--- test/check/check.go
+++ test/check/check.go
@@ -94,6 +94,26 @@ func CheckConfigCondition(conditionType ConditionType, conditionStatus Condition
 	}
 }
 
+func PlacementListFromComponentDaemonSets(component Component) ([]cnao.Placement, error) {
+	placementList := []cnao.Placement{}
+	for _, daemonSetName := range component.DaemonSets {
+		daemonSet := appsv1.DaemonSet{}
+		err := framework.Global.Client.Get(context.Background(), types.NamespacedName{Name: daemonSetName, Namespace: components.Namespace}, &daemonSet)
+		if err != nil {
+			return placementList, err
+		}
+
+		daemonSetPlacement := cnao.Placement{}
+		daemonSetPlacement.NodeSelector = daemonSet.Spec.Template.Spec.NodeSelector
+		daemonSetPlacement.Affinity = *daemonSet.Spec.Template.Spec.Affinity
+		daemonSetPlacement.Tolerations = daemonSet.Spec.Template.Spec.Tolerations
+
+		placementList = append(placementList, daemonSetPlacement)
+	}
+
+	return placementList, nil
+}
+
 func CheckConfigVersions(operatorVersion, observedVersion, targetVersion string, timeout, duration time.Duration) {
 	By(fmt.Sprintf("Checking that status contains expected versions Operator: %q, Observed: %q, Target: %q", operatorVersion, observedVersion, targetVersion))
 	config := &cnaov1.NetworkAddonsConfig{}
