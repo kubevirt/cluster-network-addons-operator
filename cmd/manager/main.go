620337c2b09a12d07a61fbf4be8a07a620097eef
Nahshon Unna Tsameret <60659093+nunnatsa@users.noreply.github.com>
2020-12-23 09:14:49 +0200

    Stop Using Deprecated Packages (#707)
    * Stop Using Deprecated Packages
    
    sigs.k8s.io/controller-runtime/pkg/runtime/* packages are deprecated, and were moved to new pathes.
    
    Trying to upgrade sigs.k8s.io/controller-runtime to version v0.7.0 in HCO created a conflict because in v0.7.0 the deprecated packages were removed and cannot be used.
    
    This PR replaces the deprecated packages with their new pathes.
    
    Signed-off-by: Nahshon Unna-Tsameret <nunnatsa@redhat.com>
    
    * Ran go mod tidy & vendor
    
    Signed-off-by: Nahshon Unna-Tsameret <nunnatsa@redhat.com>

--- cmd/manager/main.go
+++ cmd/manager/main.go
@@ -20,7 +20,7 @@ import (
 	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
 	"sigs.k8s.io/controller-runtime/pkg/client/config"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
-	"sigs.k8s.io/controller-runtime/pkg/runtime/signals"
+	"sigs.k8s.io/controller-runtime/pkg/manager/signals"
 
 	"github.com/kubevirt/cluster-network-addons-operator/pkg/apis"
 	"github.com/kubevirt/cluster-network-addons-operator/pkg/controller"
