a522ad4cbf2c5c6cb25c3a7bb057674d1b5319a7
Ram Lavi <ralavi@redhat.com>
2021-01-07 14:26:07 +0200

    unit-test, Remove copy to artifact in script
    We don't copy anything to artifacts folder during the unit test so
    better remove it.
    
    Signed-off-by: Ram Lavi <ralavi@redhat.com>

--- automation/check-patch.unit-test.sh
+++ automation/check-patch.unit-test.sh
@@ -4,14 +4,9 @@
 # cluster on any environment with basic dependencies listed in
 # check-patch.packages installed and docker running.
 
-teardown() {
-    cp $(find . -name "*junit*.xml") $ARTIFACTS
-}
-
 main() {
     source automation/check-patch.setup.sh
     cd ${TMP_PROJECT_PATH}
-    trap teardown EXIT SIGINT SIGTERM SIGSTOP
     make bump-all
     make check
     make docker-build
