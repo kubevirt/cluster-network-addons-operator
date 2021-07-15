979135e25356f0aa7272b138cd2fbd4faeb94d2d
oscollabus <oshoval@redhat.com>
2021-07-15 11:23:13 +0300

    [release-0.53], macvtap, Bump to v0.5.1 (#932)
    Signed-off-by: Or Shoval <oshoval@redhat.com>

--- pkg/components/components.go
+++ pkg/components/components.go
@@ -33,7 +33,7 @@ const (
 	NMStateHandlerImageDefault    = "quay.io/nmstate/kubernetes-nmstate-handler@sha256:a43e4132ace4e2f1752b927bfb2cff98ddba9c9c897fa5a886bf288881710e9a"
 	OvsCniImageDefault            = "quay.io/kubevirt/ovs-cni-plugin@sha256:52a303ac34cc5d7739a11d49c397a93e0653433cf58ea0d66d8dd7a0a622c543"
 	OvsMarkerImageDefault         = "quay.io/kubevirt/ovs-cni-marker@sha256:0bbe71b5f3e1ba2be0d773a54562c2758bad2c77fa8e65755896a8f40bd2de03"
-	MacvtapCniImageDefault        = "quay.io/kubevirt/macvtap-cni@sha256:375af0a38e07281d6018ad92a63d115f3e78f2b30d53f728891073ac0510cee9"
+	MacvtapCniImageDefault        = "quay.io/kubevirt/macvtap-cni@sha256:e34cc796dee2e300f866d6f5b563361253ce89226eaf9eb0c3bc792f5481b8df"
 )
 
 type AddonsImages struct {
