6520e1391761359d9ee1c994f2a175e2b85ac9b7
Edward Haas <edwardh@redhat.com>
2021-08-25 18:25:22 +0300

    components: Update CNI Bridge with macspoofchk source (#985)
    * components: Update CNI Bridge with macspoofchk source
    
    Replace the source branch of the CNI Bridge with the macspoofchk
    support, from the PR branch to a separate branch.
    
    Unlike the PR branch, the new branch is not expected to change. But even
    if it does (for fixes), history will be preserved.
    
    Signed-off-by: Edward Haas <edwardh@redhat.com>
    
    * Rebuild linux-bridge image
    
    Signed-off-by: Petr Horáček <phoracek@redhat.com>
    
    Co-authored-by: Petr Horáček <phoracek@redhat.com>

--- pkg/components/components.go
+++ pkg/components/components.go
@@ -29,7 +29,7 @@ var (
 
 const (
 	MultusImageDefault            = "quay.io/kubevirt/cluster-network-addon-multus@sha256:32867c73cda4d605651b898dc85fea67d93191c47f27e1ad9e9f2b9041c518de"
-	LinuxBridgeCniImageDefault    = "quay.io/kubevirt/cni-default-plugins@sha256:828df9e296db6f2772e69671189c68b69c258fdc9c84b4cc08b16866f69c162d"
+	LinuxBridgeCniImageDefault    = "quay.io/kubevirt/cni-default-plugins@sha256:d211830c45bde8608befb04cf2accc91490467f3b8e091b812b76453572c9ab8"
 	LinuxBridgeMarkerImageDefault = "quay.io/kubevirt/bridge-marker@sha256:8e9de03dbf6f35eb88af94e3d93da66d926d002c1574619f63186f1e88bf3c52"
 	KubeMacPoolImageDefault       = "quay.io/kubevirt/kubemacpool@sha256:4e1a54bf29456fd020cd8cbe9ea4b36e84f7f29eb342a469f6fba72da4b450a1"
 	NMStateHandlerImageDefault    = "quay.io/nmstate/kubernetes-nmstate-handler@sha256:18041db3cb27c673121e798b090e54cc42f6591f877ff599fa09f55c6d2388fe"
