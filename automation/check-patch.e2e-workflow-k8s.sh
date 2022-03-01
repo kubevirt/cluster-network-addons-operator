8d9b589c02391b5723f8c7b6bb5c0285d1ed9c42
Enrique Llorente Pastora <ellorent@redhat.com>
2022-03-01 13:32:07 +0100

    kubevirtci: Bump to k8s-1.23 (#1214)
    CNAO is testing towards k8s-1.21 we should go to the latest k8s version
    at main.
    
    Signed-off-by: Quique Llorente <ellorent@redhat.com>

--- automation/check-patch.e2e-workflow-k8s.sh
+++ automation/check-patch.e2e-workflow-k8s.sh
@@ -14,7 +14,7 @@ teardown() {
 }
 
 main() {
-    export KUBEVIRT_PROVIDER='k8s-1.21'
+    export KUBEVIRT_PROVIDER='k8s-1.23'
 
     source automation/check-patch.setup.sh
     cd ${TMP_PROJECT_PATH}
