e210b48f0d7a384c98d44b0de6470b7c1d46184a
Or Shoval <oshoval@redhat.com>
2022-07-18 13:56:33 +0300

    cleanup: Remove deprecated kubevirtci providers support
    Signed-off-by: Or Shoval <oshoval@redhat.com>

--- cluster/up.sh
+++ cluster/up.sh
@@ -27,13 +27,6 @@ cluster::install
 
 $(cluster::path)/cluster-up/up.sh
 
-if [[ "$KUBEVIRT_PROVIDER" =~ (ocp|okd)- ]]; then
-    echo 'Remove components we do not need to save some resources'
-    ./cluster/kubectl.sh delete ns openshift-monitoring --wait=false
-    ./cluster/kubectl.sh delete ns openshift-marketplace --wait=false
-    ./cluster/kubectl.sh delete ns openshift-cluster-samples-operator --wait=false
-fi
-
 if [[ "$KUBEVIRT_PROVIDER" =~ k8s- ]]; then
     echo 'Installing Open vSwitch'
     for node in $(./cluster/kubectl.sh get nodes --no-headers | awk '{print $1}'); do
