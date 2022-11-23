61ae47cb65dc133f9713a89e7995f163c4188d68
oscollabus <oshoval@redhat.com>
2022-11-23 18:42:39 +0200

    Bump kubevirtci (#1456)
    In order to consume the latest changes of DNS
    https://github.com/kubevirt/kubevirtci/pull/903
    
    Signed-off-by: Or Shoval <oshoval@redhat.com>
    
    Signed-off-by: Or Shoval <oshoval@redhat.com>

--- cluster/cluster.sh
+++ cluster/cluster.sh
@@ -13,7 +13,7 @@
 # limitations under the License.
 
 export KUBEVIRT_PROVIDER=${KUBEVIRT_PROVIDER:-'k8s-1.23'}
-export KUBEVIRTCI_TAG='2211021552-8cca8c0'
+export KUBEVIRTCI_TAG='2211212125-021efaa'
 
 KUBEVIRTCI_REPO='https://github.com/kubevirt/kubevirtci.git'
 # The CLUSTER_PATH var is used in cluster folder and points to the _kubevirtci where the cluster is deployed from.
