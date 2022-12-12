6eb35df85fb845328413ef002190ca69fec84a78
oscollabus <oshoval@redhat.com>
2022-12-12 11:05:29 +0200

    components: Remove double export (#1476)
    KUBEVIRT_PROVIDER is already exported at the sourced
    cluster.sh, no need to override it.
    It just confuse and harder to maintain because it will
    ignore the cluster.sh value which is the main one we use.
    
    Signed-off-by: Or Shoval <oshoval@redhat.com>
    
    Signed-off-by: Or Shoval <oshoval@redhat.com>

--- automation/components-functests.setup.sh
+++ automation/components-functests.setup.sh
@@ -22,7 +22,6 @@ source hack/components/yaml-utils.sh
 source cluster/cluster.sh
 
 # Spin up Kubernetes cluster
-export KUBEVIRT_PROVIDER='k8s-1.23'
 make cluster-down cluster-up
 
 # Export .kubeconfig full path, so it will be possible
