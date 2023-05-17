519a9d05d9bf47c85bad82baba4eea8dfd5c6790
Enrique Llorente <ellorent@redhat.com>
2023-05-17 12:08:39 +0200

    kubemacpool: Adapt bumper to to v0.41.0
    Latest kubemacpool has some changes that breaks autobumper, this change
    adapt autobumper and do a manual bump and change components.yaml to
    point to v0.41.0
    
    Signed-off-by: Enrique Llorente <ellorent@redhat.com>

--- automation/check-patch.e2e-kubemacpool-functests.sh
+++ automation/check-patch.e2e-kubemacpool-functests.sh
@@ -54,7 +54,7 @@ main() {
     cd ${TMP_COMPONENT_PATH}
 
     export CLUSTER_ROOT_DIRECTORY=${TMP_PROJECT_PATH}
-    KUBECONFIG=${KUBECONFIG} E2E_TEST_ARGS="--junit-output=$ARTIFACTS/junit.functest.xml --ginkgo.skip $SKIPPED_TESTS" make functest
+    KUBECONFIG=${KUBECONFIG} make E2E_TEST_EXTRA_ARGS="-ginkgo.noColor -test.outputdir=$ARTIFACTS --ginkgo.junit-report=$ARTIFACTS/junit.functest.xml --ginkgo.skip $SKIPPED_TESTS" functest
 }
 
 [[ "${BASH_SOURCE[0]}" == "$0" ]] && main "$@"
