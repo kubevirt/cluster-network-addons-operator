0f06bbf4c71f48ce3b7ac59f8f3e99f58fc6c745
Enrique Llorente Pastora <ellorent@redhat.com>
2023-05-19 09:20:45 +0200

    kubemacpool: Bump to v0.41.0 (#1551)
    * kubemacpool: Adapt bumper to to v0.41.0
    
    Latest kubemacpool has some changes that breaks autobumper, this change
    adapt autobumper and do a manual bump and change components.yaml to
    point to v0.41.0
    
    Signed-off-by: Enrique Llorente <ellorent@redhat.com>
    
    * kubemacpool: Call make bump-kubemacpool
    
    This change contains the generated stuff from calling make
    bump-kubemacpool.
    
    Signed-off-by: Enrique Llorente <ellorent@redhat.com>
    
    ---------
    
    Signed-off-by: Enrique Llorente <ellorent@redhat.com>

--- hack/components/bump-kubemacpool.sh
+++ hack/components/bump-kubemacpool.sh
@@ -32,9 +32,29 @@ bases:
 patchesStrategicMerge:
 - cnao_kubemacpool_manager_patch.yaml
 - cnao_cert-manager_patch.yaml
-- cnao_placement_patch.yaml
 - mutatevirtualmachines_opt_mode_patch.yaml
 - mutatepods_opt_mode_patch.yaml
+patches:
+- path: cnao_placement_patch.yaml
+  target:
+    group: apps
+    version: v1
+    kind: Deployment
+    name: cert-manager
+    namespace: system
+- path: cnao_placement_patch.yaml
+  target:
+    group: apps
+    version: v1
+    kind: Deployment
+    name: mac-controller-manager
+    namespace: system
+- path: cnao_mac-range_patch.json
+  target:
+    version: v1
+    kind: ConfigMap
+    name: mac-range-config
+    namespace: system
 EOF
 
     cat <<EOF > config/cnao/cnao_kubemacpool_manager_patch.yaml
@@ -69,9 +89,6 @@ metadata:
 spec:
   template:
     spec:
-      affinity: AFFINITY
-      nodeSelector: NODE_SELECTOR
-      tolerations: TOLERATIONS
       containers:
       - image: "{{ .KubeMacPoolImage }}"
         imagePullPolicy: "{{ .ImagePullPolicy }}"
@@ -88,18 +105,26 @@ spec:
 EOF
 
     cat <<EOF > config/cnao/cnao_placement_patch.yaml
-apiVersion: apps/v1
-kind: Deployment
-metadata:
-  name: mac-controller-manager
-  namespace: system
-spec:
-  template:
-    spec:
-      affinity: AFFINITY
-      nodeSelector: NODE_SELECTOR
-      tolerations: TOLERATIONS
+- op: replace
+  path: /spec/template/spec/affinity
+  value: "{{ toYaml .Placement.Affinity | nindent 8 }}"
+- op: replace
+  path: /spec/template/spec/nodeSelector
+  value: "{{ toYaml .Placement.NodeSelector | nindent 8 }}"
+- op: replace
+  path: /spec/template/spec/tolerations
+  value: "{{ toYaml .Placement.Tolerations | nindent 8 }}"
 EOF
+
+    cat <<EOF > config/cnao/cnao_mac-range_patch.json
+- op: replace
+  path: /data/RANGE_START
+  value: "{{ .RangeStart }}"
+- op: replace
+  path: /data/RANGE_END
+  value: "{{ .RangeEnd }}"
+EOF
+
 
     (
         cd config/cnao
@@ -113,16 +138,14 @@ EOF
 )
 
 rm -rf data/kubemacpool/*
+kustomize_version=$(grep kustomize $KUBEMACPOOL_PATH/Makefile |sed "s/.*@v/v/g")
+curl -L "https://github.com/kubernetes-sigs/kustomize/releases/download/kustomize%2F${kustomize_version}/kustomize_${kustomize_version}_linux_amd64.tar.gz" -o kustomize.tar.gz
+tar -xvzf kustomize.tar.gz
+mv kustomize $KUBEMACPOOL_PATH
+rm kustomize.tar.gz
 (
     cd $KUBEMACPOOL_PATH
-    make tools 1>/dev/null
-    ./build/_output/bin/go/bin/kustomize build config/cnao | \
-        sed 's/kubemacpool-system/{{ .Namespace }}/' | \
-        sed 's/RANGE_START: .*/RANGE_START: {{ .RangeStart }}/' | \
-        sed 's/RANGE_END: .*/RANGE_END: {{ .RangeEnd }}/' | \
-        sed 's/AFFINITY/{{ toYaml .Placement.Affinity | nindent 8 }}/' | \
-        sed 's/NODE_SELECTOR/{{ toYaml .Placement.NodeSelector | nindent 8 }}/' | \
-        sed 's/TOLERATIONS/{{ toYaml .Placement.Tolerations | nindent 8 }}/'
+    ./kustomize build config/cnao | sed "s/'{{ toYaml \(.*\)}}'/{{ toYaml \1}}/"
 ) > data/kubemacpool/kubemacpool.yaml
 
 echo 'Get kubemacpool image name and update it under CNAO'
--- pkg/components/components.go
+++ pkg/components/components.go
@@ -34,7 +34,7 @@ const (
 	MultusDynamicNetworksImageDefault = "ghcr.io/k8snetworkplumbingwg/multus-dynamic-networks-controller@sha256:dee1979d92f0a31598a6e3569ac7004be7d29e7ca9e31db23753ef263110dc04"
 	LinuxBridgeCniImageDefault        = "quay.io/kubevirt/cni-default-plugins@sha256:406b43253fb5d45f50d1543879353822e3f746e2794b65ab30754e800386b76d"
 	LinuxBridgeMarkerImageDefault     = "quay.io/kubevirt/bridge-marker@sha256:5d24c6d1ecb0556896b7b81c7e5260b54173858425777b7a84df8a706c07e6d2"
-	KubeMacPoolImageDefault           = "quay.io/kubevirt/kubemacpool@sha256:0cc5ad824fc163d6dea5e9bd872467c691eaa9a88944008b5d746495b2a72214"
+	KubeMacPoolImageDefault           = "quay.io/kubevirt/kubemacpool@sha256:afba7d0c4a95d2d4924f6ee6ef16bbe59117877383819057f01809150829cb0c"
 	OvsCniImageDefault                = "quay.io/kubevirt/ovs-cni-plugin@sha256:5f7290e2294255ab2547c3b4bf48cc2d75531ec5a43e600366e9b2719bef983f"
 	MacvtapCniImageDefault            = "quay.io/kubevirt/macvtap-cni@sha256:434420511e09b2b5ede785a2c9062b6658ffbc26fbdd4629ce06110f9039c600"
 	KubeRbacProxyImageDefault         = "quay.io/openshift/origin-kube-rbac-proxy@sha256:baedb268ac66456018fb30af395bb3d69af5fff3252ff5d549f0231b1ebb6901"
