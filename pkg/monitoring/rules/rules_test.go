3851bc9e796ad20c49bba33f4957a00d7e8eb35d
Assaf Admi <aadmi@redhat.com>
2024-03-28 13:00:30 +0200

    Add operator-observability rules unit test
    Add rules unit test, which ensures that alerts and recording rules
    definitions are following conventions.
    
    Signed-off-by: assafad <aadmi@redhat.com>

---
+++ pkg/monitoring/rules/rules_test.go
@@ -0,0 +1,37 @@
+package rules_test
+
+import (
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+
+	"github.com/machadovilaca/operator-observability/pkg/testutil"
+
+	"github.com/kubevirt/cluster-network-addons-operator/pkg/monitoring/rules"
+)
+
+var _ = Describe("Rules Validation", func() {
+	var linter *testutil.Linter
+
+	BeforeEach(func() {
+		Expect(rules.SetupRules("")).To(Succeed())
+		linter = testutil.New()
+	})
+
+	It("Should validate alerts", func() {
+		linter.AddCustomAlertValidations(
+			testutil.ValidateAlertNameLength,
+			testutil.ValidateAlertRunbookURLAnnotation,
+			testutil.ValidateAlertHealthImpactLabel,
+			testutil.ValidateAlertPartOfAndComponentLabels)
+
+		alerts := rules.ListAlerts()
+		problems := linter.LintAlerts(alerts)
+		Expect(problems).To(BeEmpty())
+	})
+
+	It("Should validate recording rules", func() {
+		recordingRules := rules.ListRecordingRules()
+		problems := linter.LintRecordingRules(recordingRules)
+		Expect(problems).To(BeEmpty())
+	})
+})
