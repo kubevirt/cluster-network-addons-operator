6f379eb98a4f45fa2c61466a0b77280ecf86f50e
Or Shoval <oshoval@redhat.com>
2024-07-24 09:29:55 +0300

    e2e,workflow: Increase condition waiting timeout
    We wait one minute and expect the config status to be Progressing.
    In the first time we deploy, images should be pulled,
    and then some resources fail to be created because they need a secret
    that is not created yet, and will auto retry later.
    
    The fact that they failed change the state from Progressing to Failed,
    waiting more allows giving eventually consistent to do its magic,
    and it will overcome this flakiness.
    
    Signed-off-by: Or Shoval <oshoval@redhat.com>

--- test/e2e/workflow/deployment_test.go
+++ test/e2e/workflow/deployment_test.go
@@ -481,7 +481,7 @@ func checkConfigChange(gvk schema.GroupVersionKind, components []Component, whil
 		CheckConfigCondition(gvk, ConditionAvailable, ConditionTrue, 5*time.Minute, CheckDoNotRepeat)
 	} else {
 		// If there are any components to deploy wait until Progressing condition is reported
-		CheckConfigCondition(gvk, ConditionProgressing, ConditionTrue, time.Minute, CheckDoNotRepeat)
+		CheckConfigCondition(gvk, ConditionProgressing, ConditionTrue, 5*time.Minute, CheckDoNotRepeat)
 		// Wait until Available condition is reported. It may take a few minutes the first time
 		// we are pulling component images to the Node
 		CheckConfigCondition(gvk, ConditionAvailable, ConditionTrue, 15*time.Minute, CheckDoNotRepeat)
