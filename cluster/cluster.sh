1a681c583fc61fef5d20f48c7cc36d71035d3473
Ram Lavi <ralavi@redhat.com>
2024-09-26 10:28:04 +0300

    cluster: Bump KUBEVIRT_PROVIDER to k8s-1.30
    Signed-off-by: Ram Lavi <ralavi@redhat.com>

--- cluster/cluster.sh
+++ cluster/cluster.sh
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-export KUBEVIRT_PROVIDER=${KUBEVIRT_PROVIDER:-'k8s-1.28'}
+export KUBEVIRT_PROVIDER=${KUBEVIRT_PROVIDER:-'k8s-1.30'}
 export KUBEVIRTCI_TAG=${KUBEVIRTCI_TAG:-2409241245-d93dec16}
 
 KUBEVIRTCI_REPO='https://github.com/kubevirt/kubevirtci.git'
