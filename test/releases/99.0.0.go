59696815f39b561781375d2c8f027fb26b1c8a87
Or Shoval <oshoval@redhat.com>
2025-03-12 12:48:00 +0200

    [release-0.95] bridge marker: Bump to 0.12.1
    Signed-off-by: Or Shoval <oshoval@redhat.com>

--- test/releases/99.0.0.go
+++ test/releases/99.0.0.go
@@ -30,7 +30,7 @@ func init() {
 				ParentName: "bridge-marker",
 				ParentKind: "DaemonSet",
 				Name:       "bridge-marker",
-				Image:      "quay.io/kubevirt/bridge-marker@sha256:18d954d58b9830738df9bf5c9a575d22b33096d1af26fb6bc2da09fb31c9f73a",
+				Image:      "quay.io/kubevirt/bridge-marker@sha256:8c40e9e78167472c6840a74eefa00ab001279e9c1630908e5c652893f1d9fd44",
 			},
 			{
 				ParentName: "kube-cni-linux-bridge-plugin",
