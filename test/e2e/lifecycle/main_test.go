494548f91de4121922b27e39ec42e47b49d657fd
Ram Lavi <ralavi@redhat.com>
2025-03-25 15:12:19 +0200

    tests/e2e/lifecycle,workflow: Introduce new reporter to tests
    Signed-off-by: Ram Lavi <ralavi@redhat.com>

--- test/e2e/lifecycle/main_test.go
+++ test/e2e/lifecycle/main_test.go
@@ -1,19 +1,27 @@
 package test
 
 import (
+	"fmt"
 	"os"
 	"testing"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 
+	"github.com/kubevirt/cluster-network-addons-operator/pkg/components"
 	. "github.com/kubevirt/cluster-network-addons-operator/test/check"
 	testenv "github.com/kubevirt/cluster-network-addons-operator/test/env"
 	. "github.com/kubevirt/cluster-network-addons-operator/test/operations"
 	. "github.com/kubevirt/cluster-network-addons-operator/test/releases"
+	"github.com/kubevirt/cluster-network-addons-operator/test/reporter"
 )
 
+var cnaoReporter *reporter.KubernetesCNAOReporter
+
 func TestE2E(t *testing.T) {
+	cnaoReporter = reporter.New("_out/e2e/lifecycle/", components.Namespace)
+	cnaoReporter.Cleanup()
+
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "lifecycle Test Suite")
 }
@@ -24,6 +32,13 @@ var _ = BeforeSuite(func() {
 	os.Chdir("../../../")
 
 	testenv.Start()
+})
+
+var _ = JustAfterEach(func() {
+	if CurrentSpecReport().Failed() {
+		failureCount := cnaoReporter.DumpLogs()
+		By(fmt.Sprintf("Test failed, collected logs and artifacts, failure count %d", failureCount))
+	}
 })
 
 var _ = AfterEach(func() {
