fadd340f77f2318b225d8db84c665450e7d4d282
Or Shoval <oshoval@redhat.com>
2025-10-15 16:41:24 +0300

    Bump Kubevirtci (#2423)
    * Bump Kubevirtci
    
    Signed-off-by: Or Shoval <oshoval@redhat.com>
    
    * kubevirtci: Use Flannel
    
    Signed-off-by: Or Shoval <oshoval@redhat.com>
    
    ---------
    
    Signed-off-by: Or Shoval <oshoval@redhat.com>

--- cluster/cluster.sh
+++ cluster/cluster.sh
@@ -12,8 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-export KUBEVIRT_PROVIDER=${KUBEVIRT_PROVIDER:-'k8s-1.30'}
-export KUBEVIRTCI_TAG=${KUBEVIRTCI_TAG:-2504042236-47782438}
+export KUBEVIRT_PROVIDER=${KUBEVIRT_PROVIDER:-'k8s-1.33'}
+export KUBEVIRTCI_TAG=${KUBEVIRTCI_TAG:-2509250927-f5abd488}
+export KUBEVIRT_FLANNEL=true
 
 KUBEVIRTCI_REPO='https://github.com/kubevirt/kubevirtci.git'
 # The CLUSTER_PATH var is used in cluster folder and points to the _kubevirtci where the cluster is deployed from.
