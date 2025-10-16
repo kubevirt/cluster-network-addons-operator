f94f244c3a0720b3ee767f45f166109c7044f193
Ram Lavi <ralavi@redhat.com>
2025-10-16 12:40:53 +0300

    hack/bump-version: Refactor version bump function
    In preparation to adding rc in later commit, refactor the bump version
    function - moving the version increase to the bump function for reuse.
    No logic is changed in the version bump.
    
    Signed-off-by: Ram Lavi <ralavi@redhat.com>

--- hack/bump-version.sh
+++ hack/bump-version.sh
@@ -4,23 +4,50 @@ expected_types="(major|minor|patch)"
 current_type=$1
 
 bump() {
-    version=$(hack/version.sh)
-    version_part=$(echo $version |sed $1)
-    version_part=$((++version_part))
-    version=$(echo $version | sed $2 | sed "s/version_part/$version_part/g")
-    ./hack/version.sh $version
+    local version="$1"
+    local bump_type="$2"
+
+    case $bump_type in
+        major)
+            local major=$(echo $version | sed 's/^\([0-9]\+\)\..*$/\1/')
+            major=$((++major))
+            echo "${major}.0.0"
+            ;;
+        minor)
+            local major_minor=$(echo $version | sed 's/^\([0-9]\+\)\.\([0-9]\+\)\..*$/\1.\2/')
+            local major=$(echo $major_minor | cut -d. -f1)
+            local minor=$(echo $major_minor | cut -d. -f2)
+            minor=$((++minor))
+            echo "${major}.${minor}.0"
+            ;;
+        patch)
+            local major_minor_patch=$(echo $version | sed 's/^\([0-9]\+\)\.\([0-9]\+\)\.\([0-9]\+\)$/\1.\2.\3/')
+            local major=$(echo $major_minor_patch | cut -d. -f1)
+            local minor=$(echo $major_minor_patch | cut -d. -f2)
+            local patch=$(echo $major_minor_patch | cut -d. -f3)
+            patch=$((++patch))
+            echo "${major}.${minor}.${patch}"
+            ;;
+    esac
 }
 
 bump_major() {
-   bump "s/^\(.*\)[.].*[.].*$/\1/g" "s/^.*$/version_part.0.0/g"
+    local version=$(hack/version.sh)
+    local new_version=$(bump "$version" "major")
+    ./hack/version.sh $new_version
 }
 
 bump_minor() {
-    bump "s/^.*[.]\(.*\)[.].*$/\1/g" "s/^\(.*\)[.].*[.].*$/\1.version_part.0/g"
+    local version=$(hack/version.sh)
+    local new_version=$(bump "$version" "minor")
+    ./hack/version.sh $new_version
 }
 
 bump_patch() {
-    bump "s/^.*[.].*[.]\(.*\)$/\1/g" "s/^\(.*\)[.]\(.*\)[.].*$/\1.\2.version_part/g"
+    local version=$(hack/version.sh)
+    local new_version=$(bump "$version" "patch")
+    ./hack/version.sh $new_version
+}
 }
 
 if [[ ! $current_type =~ $expected_types ]]; then
