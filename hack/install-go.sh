4609a4ce44770f455e137bfe467142c0ec9c5fc6
Ram Lavi <ralavi@redhat.com>
2025-10-30 15:22:10 +0200

    hack/install-go: Pin go installed on the CI to go.mod
    In one lane (multus-thick) also copy the helper script go-version.sh
    
    Signed-off-by: Ram Lavi <ralavi@redhat.com>

--- hack/install-go.sh
+++ hack/install-go.sh
@@ -1,7 +1,11 @@
 #!/bin/bash -xe
 
 destination=$1
-version=$(curl -s https://go.dev/dl/?mode=json | jq -r ".[0].version")
+
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+go_mod_version=$("$script_dir/go-version.sh")
+version="go${go_mod_version}"
+
 tarball=$version.linux-amd64.tar.gz
 url=https://dl.google.com/go/
 
