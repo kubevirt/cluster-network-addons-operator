5774aa304ae4ebd5395525156c0c6d14f77bd09f
Ram Lavi <ralavi@redhat.com>
2026-01-19 12:06:31 +0200

    bump KUBEVIRTCI_TAG to 2601261222-1094ed30
    Signed-off-by: Ram Lavi <ralavi@redhat.com>

--- cluster/cluster.sh
+++ cluster/cluster.sh
@@ -14,7 +14,7 @@
 
 export KUBEVIRT_FLANNEL=false
 export KUBEVIRT_PROVIDER=${KUBEVIRT_PROVIDER:-'k8s-1.33'}
-export KUBEVIRTCI_TAG=${KUBEVIRTCI_TAG:-2512231026-f475291f}
+export KUBEVIRTCI_TAG=${KUBEVIRTCI_TAG:-2601261222-1094ed30}
 
 KUBEVIRTCI_REPO='https://github.com/kubevirt/kubevirtci.git'
 # The CLUSTER_PATH var is used in cluster folder and points to the _kubevirtci where the cluster is deployed from.
