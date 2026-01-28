0921c7913fbfedf2e6d6895ae5f8c21b77415681
RamLavi <ralavi@redhat.com>
2026-01-28 11:15:46 +0200

    e2e/kubemacpool: Exclude opt-in test for vmis (#2580)
    Currently opt-in tests are excluded from running on CNAO, because it is
    not what CNAO is configuring kubemacpool to run on, so it will reconcile
    the opt-mode back, causing flakes.
    This exclusion should also be added to the new vmi opt-mode tests
    
    Excluding opt-in for vmi tests as well.
    
    Signed-off-by: Ram Lavi <ralavi@redhat.com>

--- automation/check-patch.e2e-kubemacpool-functests.sh
+++ automation/check-patch.e2e-kubemacpool-functests.sh
@@ -38,7 +38,7 @@ main() {
     cd ${TMP_COMPONENT_PATH}
 
     export CLUSTER_ROOT_DIRECTORY=${TMP_PROJECT_PATH}
-    KUBECONFIG=${KUBECONFIG} make E2E_TEST_EXTRA_ARGS="-ginkgo.noColor -test.outputdir=$ARTIFACTS --ginkgo.junit-report=$ARTIFACTS/junit.functest.xml --ginkgo.label-filter=!vm-opt-in&&!mac-range-day2-update" functest
+    KUBECONFIG=${KUBECONFIG} make E2E_TEST_EXTRA_ARGS="-ginkgo.noColor -test.outputdir=$ARTIFACTS --ginkgo.junit-report=$ARTIFACTS/junit.functest.xml --ginkgo.label-filter=!vm-opt-in&&!vmi-opt-in&&!mac-range-day2-update" functest
 }
 
 [[ "${BASH_SOURCE[0]}" == "$0" ]] && main "$@"
