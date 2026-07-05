ce06201e5cc3c6d65c695084ee7ed8fdcaec82a7
Brandon Palm <bpalm@redhat.com>
2026-07-05 03:35:32 -0500

    hack: update tls-compliance-operator to v1.0.18 (#2834)
    Update the default TLS Compliance Operator version from v0.0.10
    to v1.0.18 and add the expected PQCCompliant condition to the
    TLS compliance e2e test.
    
    Signed-off-by: Brandon Palm <bpalm@redhat.com>

--- hack/install-tls-compliance-operator.sh
+++ hack/install-tls-compliance-operator.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-VERSION=${VERSION:-v0.0.10}
+VERSION=${VERSION:-v1.0.18}
 
 readonly URL="https://github.com/sebrandon1/tls-compliance-operator/releases/download/${VERSION}/install.yaml"
 readonly NAMESPACE="tls-compliance-operator-system"
@@ -15,7 +15,7 @@ echo "Installing TLS Compliance Operator ${VERSION} from: ${URL}.."
 # By default the operator scan common TLS ports (443, 6443, 8443, 9443), and installs
 # permissive network-policy for those ports only [1].
 # To allow the operator reach and check all services the network-policy is deleted.
-# [1] https://github.com/sebrandon1/tls-compliance-operator/blob/v0.0.10/docs/troubleshooting.md#:~:text=Check%20for%20NetworkPolicy%20restrictions
+# [1] https://github.com/sebrandon1/tls-compliance-operator/blob/v1.0.18/docs/troubleshooting.md#:~:text=Check%20for%20NetworkPolicy%20restrictions
 echo "Deleting the operator's network-policy to allow egress all services.."
 ./cluster/kubectl.sh -n $NAMESPACE delete networkpolicy $NP_NAME 
 
